df3ea99c0805eeb27c2ace7537d3abc0ea38f158
Mattschoe <matthias.s.nielsen@gmail.com>
2021-02-23 09:25:59 +0100

    Yolo

--- Spil/Assets/Scripts/SceneChanger.cs
+++ Spil/Assets/Scripts/SceneChanger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VærelseTO : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
