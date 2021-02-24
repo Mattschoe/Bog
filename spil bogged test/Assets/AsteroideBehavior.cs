bda788767b7946dfad99988c38f34a0b926ed6eb
davidala1 <78424061+davidala1@users.noreply.github.com>
2021-02-24 08:56:54 +0100

    Laver videre på noget stjerne tamtam

---
+++ spil bogged test/Assets/AsteroideBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroideBehavior : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(10,20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-speed*Time.deltaTime,0);
+        if (transform.position.y <= -10) {
+            Destroy(gameObject);
+        }
+    }
+}
