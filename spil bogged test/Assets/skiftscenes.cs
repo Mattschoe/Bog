6beeecf5eaa8d8bd77fc17ce3fa65b79c75cd836
davidala1 <78424061+davidala1@users.noreply.github.com>
2021-02-25 09:09:28 +0100

    yregreger

--- spil bogged test/Assets/skiftscenes.cs
+++ spil bogged test/Assets/skiftscenes.cs
@@ -8,7 +8,7 @@ public class skiftscenes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Skift());
     }
     IEnumerator Skift() {
         yield return new WaitForSecondsRealtime(6);
