7a3d13c16959873b3f6c8abce33e6a78065bca5f
davidala1 <78424061+davidala1@users.noreply.github.com>
2021-02-25 13:23:12 +0100

    Slutninger er done

---
+++ spil bogged test/Assets/gregd.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gregd : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TilMm());
+    }
+    IEnumerator TilMm() {
+        yield return new WaitForSecondsRealtime(5);
+        SceneManager.LoadScene(0);
+    }
+}
