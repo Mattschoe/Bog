9fe91e18521973be532fbcf699e1e90bea67165c
Mattschoe <matthias.s.nielsen@gmail.com>
2021-02-25 09:09:22 +0100

    awdasawda

---
+++ spil bogged test/Assets/Assets/Scripts/Sceneswaps/VaelgRaket.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VaelgRaket : MonoBehaviour
+{
+    public void Red()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Blue()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
