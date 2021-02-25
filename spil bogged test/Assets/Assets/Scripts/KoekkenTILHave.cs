da9afb6ae1377b0b4ef39ad86517fcc7bb6874b5
davidala1 <78424061+davidala1@users.noreply.github.com>
2021-02-25 08:48:25 +0100

    changes til køkken

--- spil bogged test/Assets/Assets/Scripts/KoekkenTILHave.cs
+++ spil bogged test/Assets/Assets/Scripts/KoekkenTILHave.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class KoekkenTILHave : MonoBehaviour
 {
+    public Text tekst;
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Tissemand")
@@ -26,5 +28,25 @@ public class KoekkenTILHave : MonoBehaviour
         {
             SceneManager.LoadScene(6);
         }
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Get food")
+        {
+            tekst.enabled = true;
+        }
+        else {
+            tekst.enabled = false;
+        }
+        if (other.tag == "Get food" && Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(TilCutscene());
+            Debug.Log("Yeet");
+        }
+    }
+    IEnumerator TilCutscene() {
+        yield return new WaitForSecondsRealtime(2.5f);
+        SceneManager.LoadScene(7);
     }
 }
